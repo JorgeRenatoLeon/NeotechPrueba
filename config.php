178fc909a0ca2df261daabff41ddfd0cea09d1c9
Jorge Leon <jrleon@pucp.edu.pe>
2020-07-29 20:53:44 -0500

    VM Config

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 	session_start();
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("");
-	$gClient->setClientSecret("");
-	$gClient->setApplicationName("CPI Login Tutorial");
-	$gClient->setRedirectUri("http://localhost/GoogleLogin/g-callback.php");
+	$gClient->setClientId("1088505550775-ledne8l6cei3tad36i4un9h1dh9m3g5m.apps.googleusercontent.com");
+	$gClient->setClientSecret("IXXSFmY0qjnFoF9a4SbqZuaW");
+	$gClient->setApplicationName("Prueba Neotech Jorge");
+	$gClient->setRedirectUri("http://www.proyecto-neotech-jorge.tk/NeotechPrueba/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
