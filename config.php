c5ba233b4479c8242cf2d3015a7146b5d6198106
Jorge Leon <jrleon@pucp.edu.pe>
2020-07-30 19:35:58 -0500

    .EDU 2

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
 	$gClient->setClientId("1088505550775-ledne8l6cei3tad36i4un9h1dh9m3g5m.apps.googleusercontent.com");
 	$gClient->setClientSecret("IXXSFmY0qjnFoF9a4SbqZuaW");
 	$gClient->setApplicationName("Prueba Neotech Jorge");
-	$gClient->setRedirectUri("http://www.proyecto-neotech-jorge.tk/NeotechPrueba/g-callback.php");
+	$gClient->setRedirectUri("http://www.proyecto-neotech-jorge.tk/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 ?>
