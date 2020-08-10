cdaf17eded381ae7e519fc26a68ed085e240bb73
Andrea <andrea.reyesb@pucp.edu.pe>
2020-08-10 11:15:34 -0500

    comprobar si se encuentra en la bd

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 	session_start();
 	require_once "GoogleAPI/vendor/autoload.php";
 	$gClient = new Google_Client();
-	$gClient->setClientId("857734337574-uatr9hp91vn2al6vaoqsbts46cfn5fdi.apps.googleusercontent.com");
-	$gClient->setClientSecret("xsU6JSluKFa4kFGNZw5LIcK3");
+	$gClient->setClientId("");
+	$gClient->setClientSecret("");
 	$gClient->setApplicationName("Andrea");
 	$gClient->setRedirectUri("http://localhost:8000/g-callback.php");
 	$gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
